e27eb6dabfca63f9d52e7fd1254a56e830614788
Francisco Andaur <francisco.andaur@icloud.com>
2020-07-01 02:31:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf)# code an argument here
+  dwarf.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  # Your code here
+end
+end
+
+def summon_captain_planet(arry)# code an argument here
+    arry.map do |x|
+      puts x
+    end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(l)# code an argument here
+    l.map do |l|
+      l(0).capitalize
+    end
+    
   # Your code here
 end
 
